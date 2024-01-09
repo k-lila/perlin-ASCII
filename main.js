5883d9a59688fc36a3f5575e0db79971d90ed76a
k-lila <krishnalilapalandi@gmail.com>
2024-01-09 15:05:21 -0300

    fonte adicionada

--- main.js
+++ main.js
@@ -8,11 +8,21 @@ window.addEventListener('load', () => {
         perlinAscii.cols = Math.floor(perlinAscii.width/perlinAscii.size);
         perlinAscii.rows = Math.floor(perlinAscii.height/perlinAscii.size);
     });
+
+
+
+
     let scale_btn = document.getElementById('scale');
     scale_btn.addEventListener('change', () => {
         document.getElementById('scale-num').innerHTML = scale_btn.value
         perlinAscii.noiseScale = parseFloat(scale_btn.value);
     });
+
+
+
+
+
+    
     let increment_btn = document.getElementById('increment');
     increment_btn.addEventListener('change', () => {
         document.getElementById('increment-num').innerHTML = increment_btn.value
--- perlin_ascii.js
+++ perlin_ascii.js
@@ -9,16 +9,23 @@ var perlin_ascii = function(p) {
     p.timePassage = parseFloat(document.getElementById('time-passage').value);
     p.cols = Math.floor(p.width/p.size);
     p.rows = Math.floor(p.height/p.size);
-    // p.paleta = ['#541e35', '#df5d2e', '#ffb43e', '#a4c972', '#6bb38e'];
-    p.paleta = ['red', 'white'];
+    p.paleta = ['#541e35', '#df5d2e', '#ffb43e', '#a4c972', '#6bb38e'];
+    // p.paleta = ['red', 'white'];
     // p.paleta = ['#001219', '#005F73', '#0A9396', '#94D2BD', '#E9D8A6', '#EE9B00', '#CA6702', '#BB3E03', '#AE2012', '#9B2226'];
     p.time = 0;
     p.test = true;
     p.test_counter = 0;
+    p.font;
+
+    p.preload = function() {
+        p.font = p.loadFont('./Silkscreen-Regular.ttf')
+    }
 
     p.setup = function() {
         p.createCanvas(window.innerWidth, window.innerHeight);
         p.textAlign(p.RIGHT, p.CENTER);
+        p.textFont(p.font)
+
     }
     p.draw = function() {
         p.background(0);
@@ -31,20 +38,23 @@ var perlin_ascii = function(p) {
                 const ruido = p.noise(x_perlin * p.noiseScale, y_perlin * p.noiseScale, p.time * p.noiseScale);
                 const caracter = Math.floor(p.density.length * ruido);
                 const color = Math.floor(p.paleta.length * ruido);
-                const modifier = (p.textModifier * caracter) + 1;
+                var modifier = (p.textModifier * caracter) + 1;
+                modifier = modifier.toFixed(2);
                 p.text(p.density[caracter], x, y);
                 p.fill(p.paleta[color]);
                 p.textSize(p.size * modifier);
+                p.strokeWeight(modifier * 0.3);                
+                p.stroke(p.paleta[color]);
                 y_perlin += p.inc;
         }
         x_perlin += p.inc;
         p.time += p.timePassage;
         }
-        let fps = Math.floor(p.frameRate());
+        const fps = Math.floor(p.frameRate());
         document.getElementById('frame-counter').innerHTML = `fps: ${fps}`;
         
         if (p.test) {
-            if (fps > 15) {
+            if (fps > 10) {
                 p.size -= 1;
                 p.cols = Math.floor(p.width/p.size);
                 p.rows = Math.floor(p.height/p.size);
@@ -57,7 +67,6 @@ var perlin_ascii = function(p) {
                 p.test = false
             }
         }
-        // p.noLoop();
     }
 }
 
