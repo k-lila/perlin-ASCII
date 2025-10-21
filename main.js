553ff5c517a554f0aa3690071a7a1a7f51df9787
k-lila <krishnalilapalandi@gmail.com>
2025-10-21 20:17:42 -0300

    simplificação da função draw()

--- main.js
+++ main.js
@@ -1,61 +1,67 @@
 import { perlin_ascii } from "./perlin_ascii.js";
 function range_buttons(object) {
-    const size_btn = document.getElementById('font-size');
-    size_btn.addEventListener('change', () => {
-        document.getElementById('font-size-num').innerHTML = size_btn.value;
-        object.size = parseInt(size_btn.value);
-        object.cols = Math.floor(object.width/object.size);
-        object.rows = Math.floor(object.height/object.size);
-    });
-    const scale_btn = document.getElementById('scale');
-    scale_btn.addEventListener('change', () => {
-        document.getElementById('scale-num').innerHTML = scale_btn.value;
-        object.noiseScale = parseFloat(scale_btn.value);
-    });
-    const increment_btn = document.getElementById('increment');
-    increment_btn.addEventListener('change', () => {
-        document.getElementById('increment-num').innerHTML = increment_btn.value;
-        object.inc = parseFloat(increment_btn.value);
-    });
-    const time_btn = document.getElementById('time-passage');
-    time_btn.addEventListener('change', () => {
-        document.getElementById('time-passage-num').innerHTML = time_btn.value;
-        object.timePassage = parseFloat(time_btn.value);
-    });
+  const size_btn = document.getElementById("font-size");
+  size_btn.addEventListener("change", () => {
+    document.getElementById("font-size-num").innerHTML = size_btn.value;
+    object.size = parseInt(size_btn.value);
+    object.cols = Math.floor(object.width / object.size);
+    object.rows = Math.floor(object.height / object.size);
+  });
+  const scale_btn = document.getElementById("scale");
+  scale_btn.addEventListener("change", () => {
+    document.getElementById("scale-num").innerHTML = scale_btn.value;
+    object.noiseScale = parseFloat(scale_btn.value);
+  });
+  const increment_btn = document.getElementById("increment");
+  increment_btn.addEventListener("change", () => {
+    document.getElementById("increment-num").innerHTML = increment_btn.value;
+    object.inc = parseFloat(increment_btn.value);
+  });
+  const time_btn = document.getElementById("time-passage");
+  time_btn.addEventListener("change", () => {
+    document.getElementById("time-passage-num").innerHTML = time_btn.value;
+    object.timePassage = parseFloat(time_btn.value * 0.001);
+  });
 }
+
 function palete(object) {
-    function updateColors() {
-        var paleta = [];
-        switchers.forEach((switch_btn, index) => {
-            if (switch_btn.checked) {
-                paleta.push(selected_colors[index].value);
-            }
-        })
-        const list_a = object.paleta.join('');
-        const list_b = paleta.join('');
-        const comparison = list_a === list_b;
-        if (!comparison) {object.paleta = paleta};
+  function updateColors() {
+    var paleta = [];
+    switchers.forEach((switch_btn, index) => {
+      if (switch_btn.checked) {
+        paleta.push(selected_colors[index].value);
+      }
+    });
+    const list_a = object.paleta.join("");
+    const list_b = paleta.join("");
+    const comparison = list_a === list_b;
+    if (!comparison) {
+      object.paleta = paleta;
     }
-    const switchers =  document.querySelectorAll('.onoff-color');
-    const selected_colors = document.querySelectorAll('.color-selector');
-    for (let i = 0; i < switchers.length; i++) {
-        switchers[i].addEventListener('input', updateColors);
-        selected_colors[i].addEventListener('input', updateColors);
-    }
+  }
+  const switchers = document.querySelectorAll(".onoff-color");
+  const selected_colors = document.querySelectorAll(".color-selector");
+  for (let i = 0; i < switchers.length; i++) {
+    switchers[i].addEventListener("input", updateColors);
+    selected_colors[i].addEventListener("input", updateColors);
+  }
 }
+
 function resizeCanvas(object) {
-    window.addEventListener('resize', () => {
-        object.cols = Math.floor(window.innerWidth/object.size);
-        object.rows = Math.floor(window.innerHeight/object.size);
-        object.resizeCanvas(window.innerWidth, window.innerHeight);
-    })
+  window.addEventListener("resize", () => {
+    object.cols = Math.floor(window.innerWidth / object.size);
+    object.rows = Math.floor(window.innerHeight / object.size);
+    object.resizeCanvas(window.innerWidth, window.innerHeight);
+  });
 }
+
 function main() {
-    window.addEventListener('load', () => {
-        const perlinAscii = new p5(perlin_ascii);
-        resizeCanvas(perlinAscii);
-        range_buttons(perlinAscii);
-        palete(perlinAscii);
-    })
+  window.addEventListener("load", () => {
+    const perlinAscii = new p5(perlin_ascii);
+    resizeCanvas(perlinAscii);
+    range_buttons(perlinAscii);
+    palete(perlinAscii);
+  });
 }
-document.addEventListener('DOMContentLoaded', main);
+
+document.addEventListener("DOMContentLoaded", main);
--- perlin_ascii.js
+++ perlin_ascii.js
@@ -1,66 +1,75 @@
-var perlin_ascii = function(p) {
-    p.width = window.innerWidth;
-    p.height = window.innerHeight;
-    p.density = ' _.,-=+:;cba!?0123456789$W#@Ñ';
-    p.size = parseInt(document.getElementById('font-size').value);
-    p.textModifier = 0.5 / p.density.length;
-    p.noiseScale = parseInt(document.getElementById('scale').value);
-    p.inc = parseFloat(document.getElementById('increment').value);
-    p.timePassage = parseFloat(document.getElementById('time-passage').value);
-    p.cols = Math.floor(p.width/p.size);
-    p.rows = Math.floor(p.height/p.size);
-    p.paleta = ['#ffffff', '#eeeeee', '#8b0000', '#bf0000', '#ff0000'];
-    p.time = 0;
-    p.test = true;
-    p.test_counter = 0;
-    p.font;
-    p.preload = function() {
-        p.font = p.loadFont('./Silkscreen-Regular.ttf');
+var perlin_ascii = function (p) {
+  p.width = window.innerWidth;
+  p.height = window.innerHeight;
+  p.density = " _.,-=+:;cba!?0123456789$W#@Ñ";
+  p.size = parseInt(document.getElementById("font-size").value);
+  p.textModifier = 0.5 / p.density.length;
+  p.noiseScale = parseInt(document.getElementById("scale").value);
+  p.inc = parseFloat(document.getElementById("increment").value);
+  p.timePassage = parseFloat(
+    document.getElementById("time-passage").value * 0.001
+  );
+  p.cols = Math.floor(p.width / p.size);
+  p.rows = Math.floor(p.height / p.size);
+  p.paleta = ["#ffffff", "#eeeeee", "#8b0000", "#bf0000", "#ff0000"];
+  p.time = 0;
+  p.test = true;
+  p.test_counter = 0;
+  p.font;
+  p.frameCounterElement = document.getElementById("frame-counter");
+  p.preload = function () {
+    p.font = p.loadFont("./Silkscreen-Regular.ttf");
+  };
+  p.setup = function () {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.textAlign(p.RIGHT, p.CENTER);
+    p.textFont(p.font);
+  };
+  p.draw = function () {
+    p.background(0);
+    p.textSize(p.size * 1.25);
+
+    let x_perlin = 0;
+    let locationX = p.size;
+
+    for (let i = 0; i < p.cols; i++) {
+      let y_perlin = 0;
+      let locationY = 0;
+      for (let j = 0; j < p.rows; j++) {
+        const ruido = p.noise(
+          x_perlin * p.noiseScale,
+          y_perlin * p.noiseScale,
+          p.time * p.noiseScale
+        );
+        const caracter = Math.floor(p.density.length * ruido);
+        const color = Math.floor(p.paleta.length * ruido);
+        p.fill(p.paleta[color]);
+        p.text(p.density[caracter], locationX, locationY);
+        y_perlin += p.inc;
+        locationY += p.size;
+      }
+      x_perlin += p.inc;
+      locationX += p.size;
     }
-    p.setup = function() {
-        p.createCanvas(window.innerWidth, window.innerHeight);
-        p.textAlign(p.RIGHT, p.CENTER);
-        p.textFont(p.font);
+    p.time += p.timePassage;
+
+    const fps = Math.floor(p.frameRate());
+    p.frameCounterElement.innerHTML = fps;
+    if (p.test) {
+      if (fps > 9) {
+        p.size -= 1;
+        p.cols = Math.floor(p.width / p.size);
+        p.rows = Math.floor(p.height / p.size);
+        document.getElementById("font-size").value = p.size;
+        document.getElementById("font-size-num").innerHTML = p.size;
+      } else {
+        p.test_counter += 1;
+      }
+      if (p.test_counter == 10) {
+        p.test = false;
+      }
     }
-    p.draw = function() {
-        p.background(0)
-        let x_perlin = 0;
-        for (let i=0; i<p.cols; i++) {
-            let y_perlin = 0;
-            for (let j=0; j<p.rows; j++) {
-                const x = p.size + i*p.size;
-                const y = p.size + j*p.size;
-                const ruido = p.noise(x_perlin * p.noiseScale, y_perlin * p.noiseScale, p.time * p.noiseScale);
-                const caracter = Math.floor(p.density.length * ruido);
-                const color = Math.floor(p.paleta.length * ruido);
-                var modifier = (p.textModifier * caracter) + 1;
-                modifier = modifier.toFixed(2);
-                p.text(p.density[caracter], x, y);
-                p.fill(p.paleta[color]);
-                p.textSize(p.size * modifier);
-                p.strokeWeight(modifier * 0.5);                
-                p.stroke(p.paleta[color]);
-                y_perlin += p.inc;
-        }
-        x_perlin += p.inc;
-        p.time += p.timePassage;
-        }
-        const fps = Math.floor(p.frameRate());        
-        document.getElementById('frame-counter').innerHTML = `${fps}`;
-        if (p.test) {
-            if (fps > 9) {
-                p.size -= 1;
-                p.cols = Math.floor(p.width/p.size);
-                p.rows = Math.floor(p.height/p.size);
-                document.getElementById('font-size').value = p.size;
-                document.getElementById('font-size-num').innerHTML = p.size;
-            } else {
-                p.test_counter += 1;
-            }
-            if (p.test_counter == 10) {
-                p.test = false;
-            }
-        }
-    }
-}
+  };
+};
+
 export { perlin_ascii };
